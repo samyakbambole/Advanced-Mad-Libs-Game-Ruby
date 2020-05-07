9ba77dfc24ccd600d38258410dd93883ed022a52
samyakbambole <samyak_study@hotmail.com>
2020-05-07 21:51:00 +0530

    Updates

---
+++ game/game.rb
@@ -0,0 +1,48 @@
+# Advanced Mad Libs Game in Ruby
+#7/5/2020
+
+     puts "Enter your name: "
+     name = gets.chomp()
+
+     puts "Enter your age: "
+     age = gets.chomp()
+
+     puts "Enter a thing: "
+     thing = gets.chomp()
+
+     puts "Enter a color: "
+     color = gets.chomp()
+
+     puts "Enter your faviorite game: "
+     fav_game = gets.chomp()
+
+     puts "Enter a cartoon character: "
+     char = gets.chomp()
+
+     puts "Enter a location: "
+     location = gets.chomp()
+
+     puts "Enter your Friend's Name"
+     friend_name = gets.chomp()
+
+     puts "Enter a social media: "
+     soc_med = gets.chomp()
+
+
+     puts("Your name is " + name)
+     puts ""
+     puts ("You are " + age)
+     puts ""
+     puts ("You Like " + thing)
+     puts ""
+     puts ("You like the color " + color)
+     puts ""
+     puts ("Your Faviorite game is " + fav_game)
+     puts ""
+     puts ("Your Favorite Cartoon Character is " + char)
+     puts ""
+     puts ("Your faviorite place is " + location)
+     puts ""
+     puts ("Your Best Friend is " + friend_name)
+     puts ""
+     puts ("You are on " + soc_med) 
---
+++ game/readme.txt
@@ -0,0 +1,3 @@
+In this Repository, I will keep the mad libs game that i Modified. More Updates will be coming to it and I will be keeping it up to date at all times.
+
+Feel free to fork it, edit it and create lots of pull requests!
